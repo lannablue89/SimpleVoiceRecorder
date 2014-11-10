31478eddf67e836f1bab9ae7ff4c7b8673279263
Lan Tran <lan.tranthingoc@2359media.com.vn>
2014-11-10 17:48:47 +0700

    add permission record audio be handling error when writing file

--- SimpleVoiceRecorder-Android/app/src/main/java/com/example/lanna/simplevoicerecorder/PlaceholderFragment.java
+++ SimpleVoiceRecorder-Android/app/src/main/java/com/example/lanna/simplevoicerecorder/PlaceholderFragment.java
@@ -24,8 +24,8 @@ import java.io.IOException;
  */
 public class PlaceholderFragment extends Fragment {
 
-    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
     private static final String AUDIO_RECORDER_FILE_EXT_MP4 = ".mp4";
+    private static final String AUDIO_RECORDER_FILE_EXT_3GP = ".3gp";
     private static final String AUDIO_RECORDER_FOLDER = "AudioRecorder";
     private String file_exts[] = { AUDIO_RECORDER_FILE_EXT_MP4, AUDIO_RECORDER_FILE_EXT_3GP };
 
@@ -57,16 +57,24 @@ public class PlaceholderFragment extends Fragment {
     }
 
     private void startRecordVoice() {
-        Log.i("lanna", "start Record Voice...");
+        makeToast("start Record Voice...");
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.AudioEncoder.AMR_NB);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        String filename = getFilename();
+        if (null == filename) {
+            makeToast("no file for saving audio, stop record!!!");
+            return;
+        }
+        recorder.setOutputFile(filename);
+
         recorder.setOnErrorListener(errorListener);
         recorder.setOnInfoListener(infoListener);
-        recorder.setOutputFile(getFilename());
         try {
             recorder.prepare();
-            recorder.start();
+            recorder.start(); // Recording is now started
         } catch (IllegalStateException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -74,23 +82,43 @@ public class PlaceholderFragment extends Fragment {
         }
     }
 
+    private void makeToast(String mes) {
+         Toast.makeText(getActivity(), mes, Toast.LENGTH_SHORT).show();
+    }
+
     private void stopRecordVoice() {
-        Log.i("lanna", "stop Record Voice!");
+        makeToast("stop Record Voice!");
         if (null != recorder) {
             recorder.stop();
-            recorder.reset();
-            recorder.release();
+            recorder.reset();   // You can reuse the object by going back to setAudioSource() step
+            recorder.release(); // Now the object cannot be reused
             recorder = null;
         }
     }
 
     private String getFilename() {
         String filepath = Environment.getExternalStorageDirectory().getPath();
-        File file = new File(filepath, AUDIO_RECORDER_FOLDER);
-        if (!file.exists()) {
-            file.mkdirs();
+        File folder = new File(filepath, AUDIO_RECORDER_FOLDER);
+        if (!folder.exists()) {
+            folder.mkdirs();
+            Log.i("lanna", "folder.exists()=" + folder.exists());
         }
-        return (file.getAbsolutePath() + "/" + System.currentTimeMillis() + file_exts[currentFormat]);
+        String filename = System.currentTimeMillis() + file_exts[currentFormat];
+
+        File file = new File(folder.getAbsolutePath(), filename);
+
+        if(!file.exists()) {
+            try {
+                file.createNewFile();
+                Log.i("lanna", "created file for saving at " + file);
+                return filename;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Log.i("lanna", "fail to create file for saving at " + file);
+        return null;
     }
 
     private MediaRecorder.OnErrorListener errorListener = new MediaRecorder.OnErrorListener() {
